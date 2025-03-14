4a11748af0ed77e9dc35090ef93124fc878ff2de
e19029 <e19029@eng.pdn.ac.lk>
2025-03-14 08:10:51 +0530

    adjust bnner

--- leaderboard.py
+++ leaderboard.py
@@ -417,9 +417,9 @@ def main():
     )
 
     # col150, col151, col152 = st.columns([1, 5, 1])
-    col151 = st.columns(1)
-    with col151:
-        st.image(entity_workspace_goodluck_banner, use_column_width=True)
+    # col151 = st.columns(1)
+    # with col151:
+    st.image(entity_workspace_goodluck_banner, use_column_width=True)
 
     st.markdown("<div style='text-align: left;'>"
                 f"<h4>Select the type of data you want to view</h4>"
