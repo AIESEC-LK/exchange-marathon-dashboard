c6a3f9c4e60519d6666217fa75c4a18bb2604a37
e19029 <e19029@eng.pdn.ac.lk>
2025-03-24 23:20:28 +0530

    removal of cc cn banner

--- leaderboard.py
+++ leaderboard.py
@@ -440,7 +440,7 @@ def main():
 
     # col151 = st.columns(1)
     # with col151:
-    st.image(ccxcn_entity_workspace_goodluck_banner, use_column_width=True)
+    # st.image(ccxcn_entity_workspace_goodluck_banner, use_column_width=True)
 
     st.markdown("<div style='text-align: left;'>"
                 f"<h4>Select the type of data you want to view</h4>"
